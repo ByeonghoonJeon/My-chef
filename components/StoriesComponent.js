f4109e232b8d40f9b99de5a82b46281416166ac6
Nathan Jeon <jeon4972@snu.ac.kr>
2021-09-22 22:03:16 -0400

    Newsfeed

--- components/StoriesComponent.js
+++ components/StoriesComponent.js
@@ -3,26 +3,7 @@ import { Text, View } from "react-native";
 import { Card, Divider } from "react-native-elements";
 import { ScrollView } from "react-native-gesture-handler";
 import { STORIES } from "../shared/stories";
-
-function RenderStory({ story }) {
-  if (story) {
-    return (
-      <ScrollView>
-        <Card featuredTitle={story.title} image={story.image}>
-          <Text style={({ marginBottom: 10 }, { marginLeft: 10 })}>
-            Posted at {story.date}
-          </Text>
-          <Divider style={{ margin: 10 }} />
-          <Text style={({ marginTop: 10 }, { marginLeft: 10 })}>
-            Description
-          </Text>
-          <Text style={{ margin: 10 }}>{story.description}</Text>
-        </Card>
-      </ScrollView>
-    );
-  }
-  return <View />;
-}
+import { FlatList } from "react-native";
 
 class Stories extends Component {
   constructor(props) {
@@ -37,10 +18,69 @@ class Stories extends Component {
   };
 
   render() {
-    const storyId = this.props.navigation.getParam("storyId");
-    const story = this.state.stories.filter((story) => story.id === storyId)[0];
-    return <RenderStory story={story} />;
+    const { navigate } = this.props.navigation;
+    const renderStoriesItem = ({ item }) => {
+      return (
+        <ScrollView>
+          <Card featuredTitle={item.title} image={item.image}>
+            <Text style={({ marginBottom: 10 }, { marginLeft: 10 })}>
+              Posted at {item.date}
+            </Text>
+            <Divider style={{ margin: 10 }} />
+            <Text style={({ marginTop: 10 }, { marginLeft: 10 })}>
+              Description
+            </Text>
+            <Text style={{ margin: 10 }}>{item.description}</Text>
+          </Card>
+        </ScrollView>
+      );
+    };
+
+    return (
+      <FlatList
+        data={this.state.stories}
+        renderItem={renderStoriesItem}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    );
   }
 }
+
+// export default Directory;
+
+// class Stories extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       stories: STORIES,
+//     };
+//   }
+
+//   static navigationOptions = {
+//     title: "Our Stories",
+//   };
+
+//   render() {
+//     return (
+//       <ScrollView>
+//         <Card
+//           featuredTitle={this.state.stories[0].title}
+//           image={this.state.stories[0].image}
+//         >
+//           <Text style={({ marginBottom: 10 }, { marginLeft: 10 })}>
+//             Posted at {this.state.stories[0].date}
+//           </Text>
+//           <Divider style={{ margin: 10 }} />
+//           <Text style={({ marginTop: 10 }, { marginLeft: 10 })}>
+//             Description
+//           </Text>
+//           <Text style={{ margin: 10 }}>
+//             {this.state.stories[0].description}
+//           </Text>
+//         </Card>
+//       </ScrollView>
+//     );
+//   }
+// }
 
 export default Stories;
