0c990c9243a6adbe316624d501f5c7977acf4a13
Nathan Jeon <jeon4972@snu.ac.kr>
2021-09-24 16:44:29 -0400

    Done

--- components/PriceComponent.js
+++ components/PriceComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, Linking } from "react-native";
 import { PricingCard } from "react-native-elements";
 import * as Animatable from "react-native-animatable";
 class Pricing extends Component {
@@ -15,7 +15,8 @@ class Pricing extends Component {
             title="STANDARD"
             price="$440"
             info={["4 Guests", "2 Main dishes", "3 Side Dishes"]}
-            button={{ title: "GET STARTED", icon: "flight-takeoff" }}
+            button={{ title: " Check the availability", icon: "check" }}
+            onButtonPress={() => Linking.openURL("mailto:hoon4972@gmail.com")}
           />
         </Animatable.View>
         <Animatable.View animation="fadeInRightBig" duration={2000}>
@@ -24,7 +25,8 @@ class Pricing extends Component {
             title="DELUXE"
             price="$770"
             info={["6 Guests", "3 Main dishes", "5 Side Dishes"]}
-            button={{ title: "GET STARTED", icon: "flight-takeoff" }}
+            button={{ title: " Check the availability", icon: "check" }}
+            onButtonPress={() => Linking.openURL("mailto:hoon4972@gmail.com")}
           />
         </Animatable.View>
         <Animatable.View animation="fadeInRightBig" duration={3000}>
@@ -33,7 +35,8 @@ class Pricing extends Component {
             title="PREMIUM"
             price="$1,320"
             info={["8 Guests", "3 Main dishes", "5 Side Dishes"]}
-            button={{ title: "GET STARTED", icon: "flight-takeoff" }}
+            button={{ title: " Check the availability", icon: "check" }}
+            onButtonPress={() => Linking.openURL("mailto:hoon4972@gmail.com")}
           />
         </Animatable.View>
       </ScrollView>
